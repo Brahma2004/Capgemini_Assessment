a03e843e59b4ef7a3703f0f3bbd2999595b89876
Brahma <kbrahma114@gmail.com>
2026-09-16 11:14:04 +0530

    Added Day8 assessment,POM and HRM files

---
+++ src/main/java/POM/POMLogin.java
@@ -0,0 +1,41 @@
+package POM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class POMLogin {
+
+    WebDriver driver;
+
+    public POMLogin(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//input[@name='username']")
+    private WebElement uname;
+
+    @FindBy(xpath = "//input[@name='password']")
+    private WebElement password;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement login;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void getUname(String value) {
+        uname.sendKeys(value);
+    }
+
+    public void getPass(String value) {
+        password.sendKeys(value);
+    }
+
+    public void getLogin() {
+        login.click();
+    }
+}
